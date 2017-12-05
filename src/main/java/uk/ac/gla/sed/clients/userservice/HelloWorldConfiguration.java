6037b39d124e65bbeb0cda396f25d2f44b80f2b8
Naji Shehab <naji.shehab@hotmail.com>
2017-12-05 01:08:02 +0000

    Initial commit

---
+++ src/main/java/uk/ac/gla/sed/clients/userservice/HelloWorldConfiguration.java
@@ -0,0 +1,10 @@
+package uk.ac.gla.sed.clients.userservice;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class HelloWorldConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
